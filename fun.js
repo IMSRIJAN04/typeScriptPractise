9081ad215d273f4fb486668a3f4eb0fd8f3ae4d5
IMSRIJAN04 <srijanerroju@gmail.com>
2020-03-31 00:13:51 +0530

    interfaces added

--- fun.js
+++ fun.js
@@ -2,3 +2,19 @@ function add(num1, num2) {
     return num1 + num2;
 }
 console.log(add(5, 10));
+function fullname(person) {
+    console.log(person.firstname + " " + person.lastname);
+}
+var p = {
+    firstname: 'srijan',
+    lastname: 'erroju'
+};
+fullname(p);
+/* interface person{
+    firstname:string;
+    lastname:string;
+}*/
+function fullname1(person) {
+    console.log(person.firstname + " " + person.lastname);
+}
+fullname1(p);
--- fun.ts
+++ fun.ts
@@ -2,3 +2,26 @@
       return num1+num2;
   }
   console.log(add(5,10));
+
+  function fullname(person:{firstname:string,lastname:string})
+  {
+    console.log(`${person.firstname} ${person.lastname}`);
+
+  }
+  let p={
+      firstname:'srijan',
+      lastname:'erroju'
+
+  };
+  fullname(p);
+
+  /* interface person{
+      firstname:string;
+      lastname:string;
+  }*/
+  function fullname1(person:person)
+  {
+    console.log(`${person.firstname} ${person.lastname}`);
+
+  }
+  fullname1(p);
